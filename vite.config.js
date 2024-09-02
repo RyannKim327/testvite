8b90dfe4742e5aa1b12af555fef2daa5d326aa03
RyannKim327 <rksesgundo123@gmail.com>
2024-09-02 10:11:15 +0800

    09-02-24 19:07

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+// plugins: [react()],
+// })
+
+export default {
+  base: "testvite"
+}
